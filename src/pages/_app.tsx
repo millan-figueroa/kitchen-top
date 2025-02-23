da940945ef76341c618c476be3fe1d274172b3c4
millan-figueroa <millan.fig@gmail.com>
2025-02-23 12:26:04 -0800

    Deletes unecessary files and front loads font into _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
+import { Phudu } from "next/font/google";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Input from "@/components/Input";
 import Header from "@/components/Header";
 
+const phudu = Phudu({ weight: "600", subsets: ["latin"] });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <main className="bg-gray-900 text-white min-h-screen">
       <Header />
       <Input />
-    </>
+    </main>
   );
 }
