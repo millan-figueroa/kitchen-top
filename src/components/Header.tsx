d88177ad1b49077f30e1f20b9aa67085671df7a4
millan-figueroa <millan.fig@gmail.com>
2025-02-23 11:55:29 -0800

    Creates and styles header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import { Phudu } from "next/font/google";
+const phudu = Phudu({ weight: "600", subsets: ["latin"] });
+
+export default function Header() {
+  return (
+    <div
+      className={`${phudu.className} flex items-center justify-center w-full h-[80px] p-10 bg-slate-300`}
+    >
+      <img
+        className="w-[40px] h-[40px] mr-2"
+        src="./spoon-and-fork.png"
+        alt="spoon and fork"
+      />
+      <h1 className="text-2xl font-black text-gray-700">Robo Chef</h1>
+    </div>
+  );
+}
---
+++ src/components/Input.tsx
@@ -0,0 +1,3 @@
+export default function Input() {
+  return <h1>This is input.</h1>;
+}
