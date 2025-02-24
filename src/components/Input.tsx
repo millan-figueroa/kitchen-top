7595f9e03a41224161c92dca131e684e20062cce
millan-figueroa <millan.fig@gmail.com>
2025-02-24 14:20:00 -0800

    Adds styling for form and il elements

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,35 @@
 import React, { JSX } from "react";
 
+const ingredients = ["Chicken", "Beef", "Pork", "Fish", "Vegetables"];
+
+const ingredientsList = ingredients.map((ingredient) => {
+  return (
+    <li key={ingredient} className="p-2">
+      {ingredient}
+    </li>
+  );
+});
 export default function Input(): JSX.Element {
   return (
-    <div className="flex justify-items-center p-10 bg-slate-100">
-      <input
-        className="w-[800] h-12 border-2 p-2 mr-2 border-gray-300 rounded-md"
-        placeholder="e.g oregano"
-      />
-      <button className="w-48 h-12 p-1 bg-gray-800 rounded-md text-sm sm:text-base lg:text-lg">
-        <span className="block sm:hidden">+ Add</span>
-        <span className="hidden sm:block">+ Add ingredient</span>
-      </button>
-    </div>
+    <main className="bg-slate-100">
+      <form className="flex justify-items-center p-10">
+        <input
+          className="w-[800] h-12 border-2 p-2 mr-2 border-gray-300 rounded-md"
+          placeholder="e.g oregano"
+        />
+        <button className="w-48 h-12 p-1 bg-gray-800 rounded-md text-sm sm:text-base lg:text-lg">
+          <span className="block sm:hidden">+ Add</span>
+          <span className="hidden sm:block">+ Add ingredient</span>
+        </button>
+      </form>
+      <div className="flex flex-col items-center justify-center mr-48 ml-48 mt-16 mb-16  bg-white">
+        <h2 className="font-black text-2xl text-gray-700 p-6">
+          Your Ingredients:
+        </h2>
+        <ul className="ml-10 p-4 list-disc text-lg font-bold text-gray-700">
+          {ingredientsList}
+        </ul>
+      </div>
+    </main>
   );
 }
