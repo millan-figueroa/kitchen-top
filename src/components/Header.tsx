71548dc3f6c20ed64c0a49a16064ab784df130a3
millan-figueroa <millan.fig@gmail.com>
2025-02-24 11:21:38 -0800

    Adds styles to input section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ const phudu = Phudu({ weight: "600", subsets: ["latin"] });
 export default function Header() {
   return (
     <div
-      className={`${phudu.className} flex items-center justify-center w-full h-[80px] p-10 bg-slate-300`}
+      className={`${phudu.className} flex items-center justify-center w-full h-[80px] p-10 bg-slate-100 border-b-2 border-gray-300`}
     >
       <img
         className="w-[40px] h-[40px] mr-2"
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,9 +2,15 @@ import React, { JSX } from "react";
 
 export default function Input(): JSX.Element {
   return (
-    <div>
-      <input placeholder="e.g oregano"></input>
-      <button>+ Add ingredient</button>
+    <div className="flex justify-items-center p-10 bg-slate-100">
+      <input
+        className="w-[800] h-12 border-2 p-2 mr-2 border-gray-300 rounded-md"
+        placeholder="e.g oregano"
+      />
+      <button className="w-48 h-12 p-1 bg-gray-800 rounded-md text-sm sm:text-base lg:text-lg">
+        <span className="block sm:hidden">+ Add</span>
+        <span className="hidden sm:block">+ Add ingredient</span>
+      </button>
     </div>
   );
 }
