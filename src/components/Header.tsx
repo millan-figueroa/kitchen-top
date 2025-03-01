6f495c6ed2dacb41d28d8b234115e5edb6b1f0bb
millan-figueroa <millan.fig@gmail.com>
2025-03-01 14:37:54 -0800

    Updates styles, standardize tailwind utilities, imports new logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,14 @@ const phudu = Phudu({ weight: "600", subsets: ["latin"] });
 export default function Header() {
   return (
     <div
-      className={`${phudu.className} flex items-center justify-center w-full h-[80px] p-10 bg-slate-100 border-b-2 border-gray-300`}
+      className={`${phudu.className} flex items-center justify-center w-full h-[100px] p-10 bg-background border-b-2 border-stroke`}
     >
       <img
-        className="w-[40px] h-[40px] mr-2"
+        className="w-[50px] h-[50px] mr-2"
         src="./spoon-and-fork.png"
         alt="spoon and fork"
       />
-      <h1 className="text-2xl font-black text-gray-700">Kitchen Top</h1>
+      <h1 className="text-3xl font-black text-headline">Kitchen Top</h1>
     </div>
   );
 }
