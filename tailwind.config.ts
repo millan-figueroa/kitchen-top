49f06b7c2f8adac29c4dd1bb95f0e685fa777910
millan-figueroa <millan.fig@gmail.com>
2025-03-01 14:35:48 -0800

    Adds global styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,18 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: "#FAFAF8", //Main page background color
+        headline: "#40515E", //Headlines
+        button: '#141413', // Main brand color, buttons, links
+        buttonText: "#ffffff", //Button text color
+        paragraph: "#475467", //Text color, primary readable content
+        stroke: '#D1D5DB', // Default border/ stroke/ input placeholder text
+        secondary: '#F0EFEB', // Containers, cards, secondary elements
+        accent: '#D17557', // Container buttons 
+        tertiary: '#FAFAF8', // Container buttons text
+        // error: '#DC2626', // Red for errors
+        // success: '#16A34A', // Green for success messages
+        // warning: '#EAB308', // Yellow for warnings
       },
     },
   },
