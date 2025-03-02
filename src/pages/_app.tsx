4d66aa23b1381d37cfd401db7f0958e9c278cb00
millan-figueroa <millan.fig@gmail.com>
2025-03-01 17:38:49 -0800

    Imports Head and adds viewport

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,12 +3,16 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Input from "@/components/Input";
 import Header from "@/components/Header";
+import Head from "next/head";
 
 const phudu = Phudu({ weight: "600", subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className="min-h-screen bg-background">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Header />
       <Input />
     </main>
