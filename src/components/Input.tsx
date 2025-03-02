a975c88cfd9004b6f28df2acd6d9266462baee4c
millan-figueroa <millan.fig@gmail.com>
2025-03-01 18:58:37 -0800

    Fixes get-recipe div to properly size for mobile screen

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -33,7 +33,7 @@ export default function Input(): JSX.Element {
             aria-label="Ingredient input"
             name="ingredient"
           />
-          <button className="w-32 h-10 md:w-48 lg:w-60 lg:h-12 p-1 bg-button text-buttonText text-sm font-bold  md:text-md lg:text-md rounded-md">
+          <button className="w-32 h-10 md:w-48 lg:w-60 lg:h-12 p-1 bg-button text-buttonText text-sm md:text-md lg:text-md rounded-md">
             <span className="block md:hidden">+ Add</span>
             <span className="hidden md:block lg:block">+ Add ingredient</span>
           </button>
@@ -42,10 +42,10 @@ export default function Input(): JSX.Element {
         {/* Ingredients List */}
         {ingredientsListItems.length ? (
           <div className="p-6 md:p-8 lg:p-10">
-            <h2 className="md:p-2 text-lg md:text-xl lg:text-2xl font-semibold text-headline">
+            <h2 className="md:p-2 text-xl md:text-xl lg:text-2xl font-semibold text-headline">
               Ingredients on hand:
             </h2>
-            <ul className="ml-4 md:ml-6 lg:ml-8 md:p-2 lg:p-4 list-disc text-lg md:text-xl lg:text-2xl text-paragraph">
+            <ul className="ml-4 md:ml-6 lg:ml-8 mt-2 md:p-2 lg:p-4 list-disc text-lg md:text-xl lg:text-2xl text-paragraph">
               {ingredientsListItems}
             </ul>
           </div>
@@ -53,15 +53,17 @@ export default function Input(): JSX.Element {
 
         {/* Get recipe container */}
         {ingredientsListItems.length > 2 && (
-          <div className="flex justify-between items-center w-[600px] ml-8 mt-10 px-12 py-8 bg-secondary text-paragraph  rounded-lg">
-            <div>
-              <h3 className="text-3xl text-headline">Ready for a recipe?</h3>
-              <p className="mt-2">
+          <div className="flex justify-between items-center w-full px-6 md:px-10 lg:px-12 p-4 md:p-6 lg:py-8 mt-8 bg-secondary text-paragraph  rounded-lg">
+            <div className="p-2 mr-4">
+              <h3 className="text-lg md:text-xl lg:text-2xl font-bold text-headline">
+                Ready for a recipe?
+              </h3>
+              <p className="mt-2 lg:mt-4">
                 Generate a recipe from your list of ingredients.
               </p>
             </div>
-            <button className="px-6 py-2 bg-accent text-lg text-tertiary rounded-md">
-              Get a recipe
+            <button className="px-4 md:px-6 lg:px-8 py-2 md:py-4 bg-accent text-sm md:text-md lg:text-md text-tertiary rounded-md">
+              Get recipe!
             </button>
           </div>
         )}
