88d6907f0f5f34cd3e7660cfc8814070fc41ffd2
millan-figueroa <millan.fig@gmail.com>
2025-03-07 14:45:12 -0800

    Moves get recipe container, ingredientsListItems, and conditional rendering to show recipe into IngredientsList component

--- src/components/IngredientsList.tsx
+++ src/components/IngredientsList.tsx
@@ -1,13 +1,20 @@
 import React, { JSX } from "react";
 
 type IngredientsListProps = {
-  ingredientsListItems: JSX.Element[];
-  setIngredients: React.Dispatch<React.SetStateAction<string[]>>;
+  ingredients: string[];
+  toggleRecipe: () => void;
+  showRecipe: boolean;
 };
 
 export default function IngredientsList({
-  ingredientsListItems,
+  ingredients,
+  toggleRecipe,
+  showRecipe,
 }: IngredientsListProps): JSX.Element {
+  const ingredientsListItems = ingredients.map((ingredient) => {
+    return <li key={ingredient}>{ingredient}</li>;
+  });
+
   return (
     <div className="px-8 py-4 md:px-6 md:py-2">
       <h2 className="md:p-2 text-xl md:text-2xl lg:text-3xl font-semibold text-headline">
@@ -16,6 +23,26 @@ export default function IngredientsList({
       <ul className="ml-4 md:ml-6 lg:ml-8 mt-2 md:p-2 lg:p-4 list-disc text-lg md:text-xl lg:text-2xl text-paragraph">
         {ingredientsListItems}
       </ul>
+
+      {/* Get recipe container */}
+      {ingredientsListItems.length > 2 && (
+        <div className="flex justify-between items-center w-full px-6 md:px-10 lg:px-12 p-4 md:p-6 lg:py-8 mt-8 bg-secondary text-paragraph  rounded-lg">
+          <div className="p-2 mr-4">
+            <h3 className="text-lg md:text-xl lg:text-2xl font-bold text-headline">
+              Ready for a recipe?
+            </h3>
+            <p className="mt-2 lg:mt-4">
+              Generate a recipe from your list of ingredients.
+            </p>
+          </div>
+          <button
+            onClick={toggleRecipe}
+            className="px-4 md:px-6 lg:px-8 py-2 md:py-4 bg-accent text-sm md:text-md lg:text-md text-tertiary rounded-md"
+          >
+            Get recipe!
+          </button>
+        </div>
+      )}
     </div>
   );
 }
