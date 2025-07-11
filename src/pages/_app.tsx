1133d93e01438607c4d7f2c028116054bbd7abe8
millan-figueroa <millan.fig@gmail.com>
2025-07-11 16:13:36 -0700

    Refactors _app.tsx and index.tsx to remove unused components and simplify structure. Integrates Main component into the Home page.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,33 +1,7 @@
-import { Phudu } from "next/font/google";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import Main from "@/components/Main";
 import Header from "@/components/Header";
 import Head from "next/head";
-
-const phudu = Phudu({ weight: "600", subsets: ["latin"] });
-
-/**
- * Challenge: clean up our code!
- * Let's make a couple new components to make things a
- * little cleaner. (Notice: I'm not suggesting what we
- * have now is bad or wrong. I'm mostly finding an excuse
- * to get in some hands-on practice 🙂)
- *
- * 1. Move the entire recipe <section> into its own
- *    ClaudeRecipe component
- * 2. Move the list of ingredients <section> into its
- *    own IngredientsList component.
- *
- * While you're considering how to structure things, consider
- * where state is, think about if it makes sense or not to
- * move it somewhere else, how you'll communicate between
- * the parent/child components, etc.
- *
- * The app should function as it currently does when you're
- * done, so there will likely be some extra work to be done
- * beyond what I've listed above.
- */
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -36,7 +10,6 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <Header />
-      {/* <Main /> */}
       <Component {...pageProps} />
     </main>
   );
