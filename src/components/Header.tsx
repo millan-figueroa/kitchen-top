e3368e2cd328b66d2d1a6fd86d5715a8070cc7f9
millan-figueroa <millan.fig@gmail.com>
2025-07-31 14:39:09 -0700

    Refactors Header component layout and styling. Adjusts padding and button styles for improved user interface.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,22 @@ const phudu = Phudu({ weight: "600", subsets: ["latin"] });
 
 export default function Header() {
   return (
-    <div
-      className={`${phudu.className} flex items-center justify-center w-full p-5 md:p-7 lg:p-10 bg-background border-b-2 border-stroke`}
-    >
-      <img
-        className="w-8 mr-2 md:w-10 lg:w-12 lg:mr-3"
-        src="./spoon-and-fork.png"
-        alt="spoon and fork"
-      />
-      <h1 className="text-xl md:text-2xl lg:text-3xl font-black text-headline">
-        Kitchen Top
-      </h1>
+    <div className="flex items-center justify-center w-full p-3 md:p-4 lg:p-7s bg-background border-b-2 border-stroke">
+      <div
+        className={`${phudu.className} flex items-center justify-start w-full p-2 md:p-4 lg:p-6`}
+      >
+        <img
+          className="w-6 mr-2 md:w-10 lg:w-12 lg:mr-3"
+          src="./spoon-and-fork.png"
+          alt="spoon and fork"
+        />
+        <h1 className="text-xl md:text-2xl lg:text-3xl font-black text-headline">
+          Kitchen Top
+        </h1>
+      </div>
       <button
-        type="submit"
-        className="h-10 md:h-12 bg-button text-buttonText text-sm md:text-md font-medium rounded-md hover:opacity-90 transition"
+        onClick={() => (window.location.href = "/login")}
+        className="px-3 py-1 bg-button text-buttonText text-sm md:text-md lg:text-lg rounded-md"
       >
         Login
       </button>
