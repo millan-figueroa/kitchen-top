7d060fda6827478ffe446121834ff12a45c250a4
belunatic <abelluvyou@yahoo.com>
2026-01-10 11:53:21 -0800

    removed firebase

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,42 +1,30 @@
 "use client";
 import React, { createContext, useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import { auth } from "../firebase/config";
-import {
-  onAuthStateChanged,
-  signOut,
-  User as FirebaseUser,
-} from "firebase/auth";
 
 export const UserContext = createContext(null);
 
 const UserContextProvider = (props) => {
-  const [user, setUser] = useState<FirebaseUser | null>(null);
-  const router = useRouter();
+	const [user, setUser] = useState(null);
+	const router = useRouter();
 
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
-      setUser(firebaseUser);
-      console.log("User state changed:", firebaseUser);
-    });
-    return () => unsubscribe();
-  }, []);
+	useEffect(() => {
+		console.log("we are in here at the context level");
+	}, []);
 
-  const handleLogout = async () => {
-    try {
-      await signOut(auth);
-      setUser(null); // Clear user state
-      router.push("/"); // redirect after logout
-    } catch (error) {
-      console.error("Logout failed:", error);
-    }
-  };
+	const handleLogout = async () => {
+		try {
+			console.log("You are logged out, sike!");
+		} catch (error) {
+			console.error("Logout failed:", error);
+		}
+	};
 
-  return (
-    <UserContext.Provider value={{ user, setUser, handleLogout }}>
-      {props.children}
-    </UserContext.Provider>
-  );
+	return (
+		<UserContext.Provider value={{ user, setUser, handleLogout }}>
+			{props.children}
+		</UserContext.Provider>
+	);
 };
 
 export default UserContextProvider;
