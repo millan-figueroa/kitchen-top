ea9c0f5fe63147d3c6bfc482cf60256369442786
belunatic <abelluvyou@yahoo.com>
2026-02-09 12:26:48 -0800

    custom signup user / register user

--- src/pages/api/signup.ts
+++ src/pages/api/signup.ts
@@ -7,27 +7,33 @@ export default async function signup(req, res) {
 		return res.status(405).end();
 	}
 
-	const { email, password, name } = req.body;
+	const { email, password } = req.body;
 
 	try {
+		//connect to DB
 		const clientPromise = await client;
-		const db = clientPromise.db(); // Specify your database name if needed
+		//select a DB
+		//in this case the DB is specified in the MONGODB_URI in env
+		const db = clientPromise.db();
 
+		//check if user exists
 		const existingUser = await db.collection("users").findOne({ email });
 		if (existingUser) {
 			return res.status(422).json({ message: "User already exists" });
 		}
 
+		//hash the password
 		const hashedPassword = await bcrypt.hash(password, 12);
 
+		//add user to the DB
 		const result = await db.collection("users").insertOne({
 			email,
-			name,
 			hashedPassword, // Store the hashed password
 			createdAt: new Date(),
 		});
 
 		res.status(201).json({ message: "User created" });
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	} catch (error) {
 		res.status(500).json({ message: "Something went wrong" });
 	}
