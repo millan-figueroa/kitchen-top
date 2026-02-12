7adf6bb40f546c1d474eed167733f4d3e3bbeb0f
belunatic <abelluvyou@yahoo.com>
2026-02-11 17:35:35 -0800

    add options for session, jwt, pages,secret and rewrote the session interface typescript

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,60 +2,77 @@ import NextAuth from "next-auth";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import CredentialsProvider from "next-auth/providers/credentials";
 import client from "@/pages/libs/db";
-import bcrypt from "bcrypt"
+import bcrypt from "bcrypt";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
 	adapter: MongoDBAdapter(client),
 	providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'Email', type: 'email' },
-        password: { label: 'Password', type: 'password' },
-      },
-        async authorize(credentials,req) {
-          
-            //credentials might be null
-            if (!credentials) {
-                throw new Error('Credentials are null')
-            }
-            //connect to mongo server
-            const clientDB = await client;
-            //specify the db in our case it in the URI
-            const db = clientDB.db()
-            //find the collection we need
-        const user = await db.collection('users').findOne({ email: credentials.email });
-        
-            //if user exists
-            if (user && user.password) {
-                const isValid = await bcrypt.compare(credentials.password, user.password)
-                if (isValid) {
-                    //return the user object
-                    return { id: user._id.toString(), email: user.email };
-                }
-            } else {
-                throw new Error("Invalid Email/Password")
-            }
-            
-            return null
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.username = user.username;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      session.user.username = token.username;
-      return session;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: '/',
-  },
-});
+		CredentialsProvider({
+			name: "Credentials",
+			credentials: {
+				email: { label: "Email", type: "email" },
+				password: { label: "Password", type: "password" },
+			},
+			async authorize(credentials, req) {
+				//credentials might be null
+				if (!credentials) {
+					throw new Error("Credentials are null");
+				}
+				//connect to mongo server
+				const clientDB = await client;
+				//specify the db in our case it in the URI
+				const db = clientDB.db();
+				//find the collection we need
+				const user = await db
+					.collection("users")
+					.findOne({ email: credentials.email });
+
+				//if user exists
+				if (user && user.password) {
+					const isValid = await bcrypt.compare(
+						credentials.password,
+						user.password,
+					);
+					if (isValid) {
+						//return the user object
+						// Any object returned will be saved in `user` property of the JWT
+						return { id: user._id.toString(), email: user.email };
+					}
+				} else {
+					// If you return null then an error will be displayed advising the user to check their details.
+					throw new Error("Invalid Email/Password");
+				}
+
+				return null;
+			},
+		}),
+	],
+	session: {
+		// Set it as jwt instead of database
+		strategy: "jwt",
+
+		// Seconds - How long until an idle session expires and is no longer valid.
+		maxAge: 24 * 60 * 60, //24 hours
+	},
+	callbacks: {
+		async jwt({ token, user }) {
+			if (user) {
+				token.id = user.id;
+				token.email = user.email;
+			}
+			console.log("jwt is: ", token);
+			return token;
+		},
+		async session({ session, token }) {
+			// Send properties to the client, like an access_token and user id from a provider.
+			session.user.id = token.id as string;
+			session.user.email = token.email as string;
+			console.log(session, " is this");
+			return session;
+		},
+	},
+	secret: process.env.NEXTAUTH_SECRET,
+	pages: {
+		signIn: "/login",
+	},
 });
