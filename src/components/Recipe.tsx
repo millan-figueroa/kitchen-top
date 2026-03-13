d9be85ab2a8cc56badad0e62597f4283365fbb1a
belunatic <abelluvyou@yahoo.com>
2026-03-13 16:29:26 -0700

    redesign the recipe component to take JSON instead of markdown

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -2,24 +2,41 @@ import React, { JSX } from "react";
 import ReactMarkdown, { Components } from "react-markdown";
 
 type RecipeProps = {
-  recipe: string;
+	recipe: string;
 };
 
 export default function Recipe({ recipe }: RecipeProps): JSX.Element {
-  console.log(recipe);
-  const components: Components = {
-    p: ({ node, ...props }) => <p className="text-paragraph" {...props} />,
-    h1: ({ node, ...props }) => (
-      <h1 className="text-headline text-3xl font-semibold" {...props} />
-    ),
-    h2: ({ node, ...props }) => (
-      <h2 className="text-headline text-2xl font-semibold" {...props} />
-    ),
-  };
+	//parse the string to JSON object
+	const recipeObj = recipe && JSON.parse(recipe);
 
-  return (
-    <section className="p-8 sm:px-10 sm:mx-16 sm:mt-10 md:px-12 md:mx-24  lg:px-14 lg:mx-32 xl:px-16 xl:mx-40  text-paragraph">
-      <ReactMarkdown components={components}>{recipe}</ReactMarkdown>
-    </section>
-  );
+	return (
+		<section className="p-8 sm:px-10 sm:mx-16 sm:mt-10 md:px-12 md:mx-24  lg:px-14 lg:mx-32 xl:px-16 xl:mx-40  text-paragraph">
+			{recipeObj ? (
+				<>
+					<h1 className="text-headline text-3xl font-semibold mb-4">
+						{recipeObj?.title}
+					</h1>
+					<h2 className="text-headline text-2xl font-semibold mb-2">
+						Ingredients
+					</h2>
+					<ul className="list-disc ml-6 mb-4">
+						{recipeObj?.ingredients.map((ing: string, index: number) => (
+							<li key={index}>{ing}</li>
+						))}
+					</ul>
+					<h2 className="text-headline text-2xl font-semibold mb-2">
+						Instructions
+					</h2>
+					<ol className="list-decimal ml-6">
+						{recipeObj?.instructions.map((step: string, index: number) => (
+							<li key={index}>{step}</li>
+						))}
+					</ol>
+					{/* <ReactMarkdown components={components}>{recipe}</ReactMarkdown> */}
+				</>
+			) : (
+				<p className="text-paragraph">No recipe found. Please try again.</p>
+			)}
+		</section>
+	);
 }
