9dae2a2be3bee9f6f17d32898b10283052cdd133
belunatic <abelluvyou@yahoo.com>
2026-03-20 14:08:16 -0700

    add inteface for Recipe

--- src/components/Recipe.tsx
+++ src/components/Recipe.tsx
@@ -1,25 +1,28 @@
 import React, { JSX } from "react";
 
+interface Recipe {
+	title: string;
+	ingredients: string[];
+	instructions: string[];
+}
+
 type RecipeProps = {
-	recipe: string;
+	recipe: Recipe;
 };
 
 export default function Recipe({ recipe }: RecipeProps): JSX.Element {
-	//parse the string to JSON object
-	const recipeObj = recipe && JSON.parse(recipe);
-
 	return (
 		<section className="p-8 sm:px-10 sm:mx-16 sm:mt-10 md:px-12 md:mx-24  lg:px-14 lg:mx-32 xl:px-16 xl:mx-40  text-paragraph">
-			{recipeObj ? (
+			{recipe ? (
 				<>
 					<h1 className="text-headline text-3xl font-semibold mb-4">
-						{recipeObj?.title}
+						{recipe?.title}
 					</h1>
 					<h2 className="text-headline text-2xl font-semibold mb-2">
 						Ingredients
 					</h2>
 					<ul className="list-disc ml-6 mb-4">
-						{recipeObj?.ingredients.map((ing: string, index: number) => (
+						{recipe?.ingredients.map((ing: string, index: number) => (
 							<li key={index}>{ing}</li>
 						))}
 					</ul>
@@ -27,7 +30,7 @@ export default function Recipe({ recipe }: RecipeProps): JSX.Element {
 						Instructions
 					</h2>
 					<ol className="list-decimal ml-6">
-						{recipeObj?.instructions.map((step: string, index: number) => (
+						{recipe?.instructions.map((step: string, index: number) => (
 							<li key={index}>{step}</li>
 						))}
 					</ol>
