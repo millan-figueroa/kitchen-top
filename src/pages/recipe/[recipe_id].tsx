13d9bfc769431e21c7ea4fb13e09a4011c3775e0
belunatic <abelluvyou@yahoo.com>
2026-04-17 14:30:25 -0700

    remove the import of useEffect, it is not in use

--- src/pages/recipe/[recipe_id].tsx
+++ src/pages/recipe/[recipe_id].tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import axios from "axios";
 import Recipe from "@/components/Recipe";
 import { useSession } from "next-auth/react";
