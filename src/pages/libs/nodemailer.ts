5a28c57fad17b37c1d6ffe154b1368306a5c2425
belunatic <abelluvyou@yahoo.com>
2026-05-08 15:19:20 -0700

    utility function for setting up nodemailer transporter object

--- src/pages/libs/nodemailer.ts
+++ src/pages/libs/nodemailer.ts
@@ -2,9 +2,10 @@ import nodemailer from "nodemailer";
 
 // reusable nodemailer setup
 export const transporter = nodemailer.createTransport({
-	host: process.env.SMTP_HOST,
-	port: Number(process.env.SMTP_PORT),
-	secure: true,
+	// host: process.env.SMTP_HOST,
+	// port: Number(process.env.SMTP_PORT),
+	// secure: true,
+	service: "gmail",
 	auth: {
 		user: process.env.SMTP_USER,
 		pass: process.env.SMTP_PASS,
